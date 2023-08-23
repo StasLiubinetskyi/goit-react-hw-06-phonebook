0829bd480b53d2a3d8a24af2533c4a8814c1e70e
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-08-23 18:37:05 +0300

    store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 
 const persistConfig = {
   key: 'root',
   storage,
+  version: 1,
+  blacklist: ['contacts'], // Додано поле, яке треба ігнорувати при persist
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
@@ -14,8 +25,12 @@ export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
-
-export default store;
